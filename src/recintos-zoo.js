5d83d9d8ddb969065e1203b29828fbe7189244d2
lucasdsmachado <briandsmachado@gmail.com>
2024-09-12 20:55:11 -0300

    [refactor] update RecintosZoo to use RecintosZooService for enclosure analysis

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,8 +1,9 @@
+import { RecintosZooService } from "./core/usecases/RecintosZooService.js";
+
 class RecintosZoo {
-
     analisaRecintos(animal, quantidade) {
+        return RecintosZooService.analisaRecintos(animal, quantidade);
     }
-
 }
 
 export { RecintosZoo as RecintosZoo };
